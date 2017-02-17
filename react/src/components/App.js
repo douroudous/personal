f72a7fc5b4fa5a5433cb48ad63a071c218f0b7e1
Dino Dourountoudakis <CDOUROUNTOUDAKIS@GMAIL.COM>
2017-02-17 14:06:56 -0500

    Added/removed files

---
+++ react/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = props => {
+  return(
+    <div>
+      TESTINGGGG
+    </div>
+  )
+};
+
+
+export default App;
--- react/src/main.js
+++ react/src/main.js
@@ -1,10 +1,13 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 // import data from './constants/data';
 
 $(function() {
-
-
+  ReactDOM.render(
+    <App />
+    document.getElementById('app')
+  );
 });
